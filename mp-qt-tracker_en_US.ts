263aa0bb2e20c3c76d45e496408835437fb52bfd
EthanArmbrust <EthanArmbrust@github.com>
2020-07-14 16:05:43 -0400

    Add icon for windows executable

--- mp-qt-tracker_en_US.ts
+++ mp-qt-tracker_en_US.ts
@@ -1,175 +1,202 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="mp-qt-tracker_en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="253"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="253"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="253"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="238"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="278"/>
-        <source>PB</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="120"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="262"/>
-        <source>Morph</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="81"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="257"/>
-        <source>Spider</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="215"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="275"/>
-        <source>WaveBus</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="180"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="270"/>
-        <source>Scan</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="106"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="260"/>
-        <source>Boost</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="229"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="277"/>
-        <source>Bombs</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="222"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="276"/>
-        <source>Ice</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="mainwindow.ui" line="38"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="254"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="254"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="254"/>
         <source>In Game Time:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="255"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="255"/>
+        <source>Missile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="256"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="256"/>
+        <source>SJ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="81"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="257"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="257"/>
+        <source>Spider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="88"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="258"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="258"/>
+        <source>Ice Sprea</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="97"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="259"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="259"/>
+        <source>Artifacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="260"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="260"/>
+        <source>Boost</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="261"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="261"/>
+        <source>Charge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="262"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="262"/>
+        <source>Morph</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="mainwindow.ui" line="127"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="263"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="263"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="263"/>
         <source>XRay</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="134"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="264"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="264"/>
+        <source>Grapple</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="mainwindow.ui" line="141"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="265"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="265"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="265"/>
         <source>Wave</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="266"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="266"/>
+        <source>Phazon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="267"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="267"/>
+        <source>Etank</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="166"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="268"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="268"/>
+        <source>Varia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="mainwindow.ui" line="173"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="269"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="269"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="269"/>
         <source>Plasma</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="180"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="270"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="270"/>
+        <source>Scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="187"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="271"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="271"/>
+        <source>Flamethr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="194"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="272"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="272"/>
+        <source>Gravity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="201"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="273"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="273"/>
+        <source>Supers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="274"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="274"/>
+        <source>Thermal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="215"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="275"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="275"/>
+        <source>WaveBus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="222"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="276"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="276"/>
+        <source>Ice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="229"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="277"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="277"/>
+        <source>Bombs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="238"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="278"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="278"/>
+        <source>PB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="mainwindow.ui" line="256"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="279"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="279"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="279"/>
         <source>Retry</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="269"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="280"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Debug/ui_mainwindow.h" line="280"/>
+        <location filename="build/mp-qt-tracker_autogen/include_Release/ui_mainwindow.h" line="280"/>
         <source>Dolphin status: Connected</source>
-        <oldsource>Dolphin status:</oldsource>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="97"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="259"/>
-        <source>Artifacts</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="74"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="256"/>
-        <source>SJ</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="88"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="258"/>
-        <source>Ice Sprea</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="201"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="273"/>
-        <source>Supers</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="65"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="255"/>
-        <source>Missile</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="157"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="267"/>
-        <source>Etank</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="208"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="274"/>
-        <source>Thermal</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="187"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="271"/>
-        <source>Flamethr</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="166"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="268"/>
-        <source>Varia</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="194"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="272"/>
-        <source>Gravity</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="148"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="266"/>
-        <source>Phazon</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="113"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="261"/>
-        <source>Charge</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="134"/>
-        <location filename="build/mp-qt-tracker_autogen/include/ui_mainwindow.h" line="264"/>
-        <source>Grapple</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
